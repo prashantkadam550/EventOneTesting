80712a3ca15d1f1ba380240379ea285958eb4ccb
Prashant Kadam <prashantkadam550@gmail.com>
2019-04-04 12:59:53 +0530

    asf

--- EventOneTask/GitTest/GitRepository.cs
+++ EventOneTask/GitTest/GitRepository.cs
@@ -17,6 +17,7 @@ namespace GitTest
             gitInfo.RedirectStandardError = true;
             gitInfo.RedirectStandardOutput = true;
             gitInfo.FileName = mGitInstalledPath;
+
         }
 
         public String gitFetchMethod()
